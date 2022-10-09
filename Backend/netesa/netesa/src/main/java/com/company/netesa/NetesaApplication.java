7dd6ba339ff8b142f439289c869df14e4d97272c
Jhon Tabordaces <Jhon.Tabordaces@Teradata.com>
2022-10-08 21:15:16 -0500

    Se crea backend netesa

---
+++ Backend/netesa/netesa/src/main/java/com/company/netesa/NetesaApplication.java
@@ -0,0 +1,13 @@
+package com.company.netesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetesaApplication.class, args);
+	}
+
+}
